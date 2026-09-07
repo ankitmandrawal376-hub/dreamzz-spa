b07a362574f579c646451e6836332bfb31e8d539
ankitmandrawal376-hub <ankitmandrawal376@gmail.com>
2026-09-07 23:10:29 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+// server.js
+const http = require("http");
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    "Content-Type": "text/plain"
+  });
+
+  res.end("AI Business Agent Server is running!");
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
